d581e76312f50e2f18281ec5055a7a0f15c42fe5
trung_kien_nguyen <kienvipdeptrai245@gmai.com>
2024-10-13 11:59:12 +0700

    file and exception handling

---
+++ src/main/java/com/javaProject/shopManagement/exception/GlobalExeptionHandler.java
@@ -0,0 +1,19 @@
+package com.javaProject.shopManagement.exception;
+
+import javafx.scene.control.Alert;
+
+public class GlobalExeptionHandler {
+
+    public static void handleException(Exception e) {
+        System.err.println("error " + e.getMessage());
+        showErrorDialog(e.getMessage());
+    }
+
+    private static void showErrorDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Program caught an error");
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
